0ee9c55a806d0aa82e554645b70585dd328197bf
Daniel Szul <amper00@gmail.com>
2024-02-21 21:49:37 +0100

    try

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,29 @@
-// Add imports above this line
-import { galleryItems } from './gallery-items';
-// Change code below this line
+
+import { galleryItems } from "./gallery-items.js";
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
+
+const gallery = document.querySelector(".gallery");
+
+const newArray = galleryItems.map(
+  (element) =>
+    `<li class="gallery__item">
+      <a class="gallery__link" href="${element.original}">
+        <img
+          class="gallery__image"
+          src="${element.preview}" 
+          alt="${element.description}"
+        />
+      </a>
+     </li>`
+);
+
+gallery.innerHTML = newArray.join("");
+
+const lightbox = new SimpleLightbox('.gallery a', {
+  captionDelay: 250,
+  captionsData: 'alt'
+});
 
 console.log(galleryItems);
--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,35 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+
+
+try {
+ player.setCurrentTime(JSON.parse(localStorage.getItem('videoplayer-current-time')))   
+} catch (error) {
+    switch (error.name) {
+        case 'RangeError':
+            "the time was less than 0 or greater than the videoâ€™s duration"
+            break;
+
+        default:
+            "some other error occurred"
+            break;
+    } 
+}
+player.on('play', function() {
+    console.log('played the video!');
+});
+
+player.getVideoTitle().then(function(title) {
+    console.log('title:', title);
+});
+
+const setTime = (currentTime) => {
+    localStorage.setItem('videoplayer-current-time', currentTime.seconds);
+}
+
+
+
+player.on('timeupdate', throttle(setTime, 1000));
